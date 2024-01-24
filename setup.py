e30fcba29c01c82c54384c6f5d417f50f5196663
MySlientWind <634790985@qq.com>
2024-01-25 00:41:20 +0800

    Add setup.py file for project configuration

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name="ait",
+      version="0.0.1",
+      author="Tao Jing",
+      author_email="jingt20@mails.tsinghua.edu.cn",
+      description="astro image toolkits",
+      packages=find_packages())

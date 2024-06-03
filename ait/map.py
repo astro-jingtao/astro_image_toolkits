3554caa313714ecddd5b7629410e571a76a87f9b
MySlientWind <634790985@qq.com>
2024-06-03 14:41:56 +0800

    per_pixel_to_per_area, check shape

--- ait/map.py
+++ ait/map.py
@@ -1,11 +1,14 @@
 import pickle
+from typing import Dict, Any
 
 import h5py
 import numpy as np
 from astropy.io import fits
 from astropy.wcs import WCS
+import astropy.units as u
 
 
+# TODO: 3d layer
 class Map:
 
     DATA_VERSION = 1
@@ -13,16 +16,20 @@ class Map:
     def __init__(self,
                  *,
                  layers,
-                 pixel_size,
+                 pixel_scale,
                  wcs=None,
                  redshift=None,
                  metadata=None) -> None:
 
-        self.layers = layers
-        self.pixel_size = pixel_size  # in arcsec
-        self.wcs = wcs
-        self.redshift = redshift
-        self.metadata = metadata
+        self.layers: Dict[
+            str, np.
+            ndarray] = layers  # if 3D, the first dimension is the additional dimension
+        self.pixel_scale: float = pixel_scale  # in arcsec
+        self.wcs: WCS = wcs
+        self.redshift: float = redshift
+        self.metadata: Dict[str, Any] = metadata
+
+        self.check_shape()
 
     @classmethod
     def load(cls, filename):
@@ -41,7 +48,8 @@ class Map:
 
             attributes = {
                 key: value
-                for key, value in file.attrs.items() if not key == 'data_version'
+                for key, value in file.attrs.items()
+                if not key == 'data_version'
             }
             metadata_bytes = bytes(file['metadata'][()])
             attributes['metadata'] = pickle.loads(metadata_bytes)
@@ -52,6 +60,39 @@ class Map:
             attributes['wcs'] = WCS(wcs_header)
 
         return cls(layers=layers, **attributes)
+
+    def check_shape(self):
+
+        def get_shape(layer):
+            return layer.shape if len(layer.shape) == 2 else layer.shape[1:]
+
+        shape = None
+        for key, value in self.layers.items():
+            if shape is None:
+                shape = get_shape(value)
+            else:
+                if shape != get_shape(value):
+                    raise ValueError(
+                        f"Shape mismatch: {shape} != {value.shape} for {key}")
+
+    def save(self, filename):
+        with h5py.File(filename, 'w') as file:
+            for key, value in self.layers.items():
+                file.create_dataset(key, data=value)
+
+            # Handle WCS specifically if it's not None
+            wcs_string = self.wcs.to_header_string() if self.wcs else None
+
+            # set attribute values
+            file.attrs['pixel_scale'] = self.pixel_scale
+            file.attrs['wcs'] = wcs_string
+            file.attrs['redshift'] = self.redshift
+            file.attrs['data_version'] = self.DATA_VERSION
+
+            # Serialize other attributes using pickle, including WCS as string
+
+            metadata_bytes = pickle.dumps(self.metadata)
+            file.create_dataset('metadata', data=np.void(metadata_bytes))
 
     def get_snr(self, name):
         name_snr = f"{name}_snr"
@@ -71,21 +112,34 @@ class Map:
             return np.abs(self.layers[name] / self.layers[name_snr])
         return None
 
-    def save(self, filename):
-        with h5py.File(filename, 'w') as file:
-            for key, value in self.layers.items():
-                file.create_dataset(key, data=value)
+    def per_pixel_to_per_area(self,
+                              name,
+                              in_unit=u.erg / u.s / u.cm**2,
+                              out_unit=u.erg / u.s / u.kpc**2,
+                              kind='value',
+                              cosmo=None):
+        '''
+        Assume the input map is flux like, and the unit of the input map is per pixel. e.g. erg/s/cm^2/pixel
+        
+        The output map is surface brightness like. e.g. erg/s/kpc^2
+        '''
 
-            # Handle WCS specifically if it's not None
-            wcs_string = self.wcs.to_header_string() if self.wcs else None
+        scaler = (4 * np.pi) / (np.deg2rad(self.pixel_scale / 3600)**2)
 
-            # set attribute values
-            file.attrs['pixel_size'] = self.pixel_size
-            file.attrs['wcs'] = wcs_string
-            file.attrs['redshift'] = self.redshift
-            file.attrs['data_version'] = self.DATA_VERSION
+        if cosmo is None:
+            D_L = 1 * u.Mpc
+            D_A = 1 * u.Mpc
+        else:
+            D_L = cosmo.luminosity_distance(self.redshift).to(u.Mpc)
+            D_A = cosmo.angular_diameter_distance(self.redshift).to(u.Mpc)
 
-            # Serialize other attributes using pickle, including WCS as string
+        scaler *= (D_L / D_A)**2
 
-            metadata_bytes = pickle.dumps(self.metadata)
-            file.create_dataset('metadata', data=np.void(metadata_bytes))
+        if kind == 'snr':
+            return self.get_snr(name)
+        elif kind == 'value':
+            data = self.layers[name]
+        elif kind == 'err':
+            data = self.get_err(name)
+
+        return (data * in_unit * scaler).to(out_unit).value
--- test/test_map.py
+++ test/test_map.py
@@ -2,6 +2,9 @@ import os
 
 import numpy as np
 from astropy.wcs import WCS
+import astropy.units as u
+from astropy.cosmology import Planck15
+import pytest
 
 from ait.map import Map
 
@@ -24,7 +27,6 @@ def generate_map_instance(with_err=True, with_snr=False):
     pixel_size = 0.5
     metadata = {'aaa': 123, 'bbb': 456}
 
-
     # https://docs.astropy.org/en/stable/wcs/example_create_imaging.html
     wcs = WCS(naxis=2)
     wcs.wcs.crpix = [-234.75, 8.3393]
@@ -33,11 +35,10 @@ def generate_map_instance(with_err=True, with_snr=False):
     wcs.wcs.ctype = ["RA---AIR", "DEC--AIR"]
     wcs.wcs.set_pv([(2, 1, 45.0)])
 
-
     redshift = 0.1
 
     return Map(layers=layers,
-               pixel_size=pixel_size,
+               pixel_scale=pixel_size,
                metadata=metadata,
                wcs=wcs,
                redshift=redshift)
@@ -51,7 +52,7 @@ class TestSaveLoad:
         _map_loaded = Map.load(TMP_PATH)
 
         assert _map.metadata == _map_loaded.metadata
-        assert _map.pixel_size == _map_loaded.pixel_size
+        assert _map.pixel_scale == _map_loaded.pixel_scale
         assert _map.redshift == _map_loaded.redshift
         assert np.allclose(_map.layers['image'], _map_loaded.layers['image'])
         assert np.allclose(_map.layers['image_err'],
@@ -60,3 +61,70 @@ class TestSaveLoad:
         )
 
         os.remove(TMP_PATH)
+
+
+class TestUnitConversion:
+
+    def test_pixel_to_area(self):
+        _map = generate_map_instance()
+
+        image_area = _map.per_pixel_to_per_area('image',
+                                                in_unit=u.erg / u.s / u.cm**2 *
+                                                1e-17)
+
+        image_err_area = _map.per_pixel_to_per_area('image',
+                                                    in_unit=u.erg / u.s /
+                                                    u.cm**2 * 1e-17,
+                                                    kind='err')
+
+        # from emilkit
+        image_true = _map.layers['image'] * 4 * np.pi / (np.deg2rad(
+            _map.pixel_scale / 3600)**2) * (3.0856776e21)**2 * 1e-17
+
+        image_err_true = _map.get_err('image') * 4 * np.pi / (np.deg2rad(
+            _map.pixel_scale / 3600)**2) * (3.0856776e21)**2 * 1e-17
+
+        assert np.allclose(image_area, image_true)
+        assert np.allclose(image_err_area, image_err_true)
+
+        # with cosmo
+        cosmo = Planck15
+
+        image_area_cosmo = _map.per_pixel_to_per_area('image',
+                                                      in_unit=u.erg / u.s /
+                                                      u.cm**2 * 1e-17,
+                                                      cosmo=cosmo)
+
+        scaler = ((cosmo.luminosity_distance(_map.redshift) /
+                   cosmo.angular_diameter_distance(
+                       _map.redshift))**2).decompose().value
+
+        assert np.allclose(image_area_cosmo, image_true * scaler)
+
+        # 3d layer
+
+        _map.layers['cube'] = np.random.rand(20, 10, 10)
+
+        cube_area = _map.per_pixel_to_per_area('cube',
+                                               in_unit=u.erg / u.s / u.cm**2 *
+                                               1e-17)
+
+        cube_true = _map.layers['cube'] * 4 * np.pi / (np.deg2rad(
+            _map.pixel_scale / 3600)**2) * (3.0856776e21)**2 * 1e-17
+
+        assert np.allclose(cube_area, cube_true)
+
+
+class TestSelfCheck:
+
+    def test_check_shape(self):
+        _map = generate_map_instance()
+        _map.check_shape()
+
+        _map.layers['image_err'] = np.random.rand(5, 5)
+        # should raise error
+        with pytest.raises(ValueError):
+            _map.check_shape()
+
+        _map.layers['image_err'] = np.random.rand(5, 10, 10)
+        _map.check_shape()
